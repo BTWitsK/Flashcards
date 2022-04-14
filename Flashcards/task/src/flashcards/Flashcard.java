efe1451099b72fe8cacdd70a9fb4f5210dd4fa0a
BTWitsK <BTWitsK@Gmail.com>
2022-04-13 19:44:50 -0600

    completed stage 3

--- Flashcards/task/src/flashcards/Flashcard.java
+++ Flashcards/task/src/flashcards/Flashcard.java
@@ -1,21 +1,20 @@
 package flashcards;
+import java.util.*;
 
 class Flashcard {
+    private static final Scanner scanner = new Scanner(System.in);
     String term;
     String definition;
+    private final String wrong;
 
-    Flashcard() {
-        term = "purchase";
-        definition = "buy";
+    Flashcard(String term, String definition) {
+        this.term = term;
+        this.definition = definition;
+        wrong = String.format("Wrong. The right answer is \"%s\"",this.definition);
     }
 
-    @Override
-    public String toString() {
-        return String.format(""" 
-                Card:
-                %s
-                Definition:
-                %s
-                """, term, definition);
+    public void isCorrect(){
+        System.out.printf("Print the definition of \"%s\":\n", this.term);
+        System.out.println(this.definition.equals(scanner.nextLine()) ? "Correct!" : wrong);
     }
 }
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,7 +1,20 @@
 package flashcards;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print(new Flashcard());
+        ArrayList<Flashcard> cardList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number of cards:");
+        int cards = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= cards; i++) {
+            System.out.printf("Card #%d\n", i);
+            String term = scanner.nextLine();
+            System.out.printf("The definition for card #%d:\n", i);
+            String definition = scanner.nextLine();
+            cardList.add(new Flashcard(term, definition));
+        }
+        cardList.forEach(Flashcard::isCorrect);
     }
 }

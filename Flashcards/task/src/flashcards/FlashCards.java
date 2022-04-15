321cf5bdf6aaa5f6d06bd3cb7af522f340ecc4a7
BTWitsK <BTWitsK@Gmail.com>
2022-04-14 20:51:26 -0600

    implemented class FlashCards and methods

--- Flashcards/task/src/flashcards/FlashCards.java
+++ Flashcards/task/src/flashcards/FlashCards.java
@@ -1,6 +1,5 @@
 package flashcards;
-import java.io.FileReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
 public class FlashCards {
@@ -22,35 +21,106 @@ public class FlashCards {
     private Scanner scanner = new Scanner(System.in);
     private LinkedHashMap<String, String> cardMap = new LinkedHashMap<>();
     private LinkedHashMap<String, String> keyMap = new LinkedHashMap<>();
-    Menu state;
-    FileReader inputFile;
-    PrintWriter outputFile;
+    private Menu state;
 
     public void setMenu(String input) {
-        //TODO: implement menu
+        for (Menu state : Menu.values()) {
+            if(state.userChoice.equals(input)) {
+                this.state = state;
+            }
+        }
     }
 
-    public Menu currentState() {
-        return state;
+    public boolean isRunning() {
+        return state != Menu.EXIT;
     }
 
     public void addCard() {
-        //TODO: implement add
+        System.out.println("The card:");
+        String term = scanner.nextLine();
+        if (cardMap.containsKey(term)) {
+            System.out.printf("The card \"%s\" already exists.\n\n", term);
+            return;
+        }
+
+        System.out.println("The definition of the card:");
+        String definition = scanner.nextLine();
+        if (cardMap.containsValue(definition)) {
+            System.out.printf("The definition \"%s\" already exists.\n\n", term);
+            return;
+        }
+
+        cardMap.put(term , definition);
+        keyMap.put(definition, term);
+        System.out.printf("The pair (\"%s\":\"%s\") has been added.\n", term, definition);
     }
 
     public void removeCard() {
-        //TODO: implement remove
+        System.out.println("Which card?");
+        String card = scanner.nextLine();
+
+        if (null == cardMap.remove(card)) {
+            System.out.printf("Can't remove \"%s\": there is no such card.\n\n", card);
+        } else {
+            System.out.println("The card has been removed.");
+        }
     }
 
     public void importCards() {
-        //TODO: implement import
+        System.out.println("File name:");
+        int cardCount = 0;
+        try {
+            scanner = new Scanner(new FileReader(scanner.nextLine()));
+            while (scanner.hasNext()) {
+                String[] card = scanner.nextLine().split("\\|");
+                cardMap.put(card[0], card[1]);
+                keyMap.put(card[1], card[0]);
+                cardCount++;
+            }
+            System.out.printf("%d cards have been loaded.\n\n", cardCount);
+            scanner = new Scanner(System.in);
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
     }
 
     public void exportCards() {
-        //TODO: implement export
+        System.out.println("File name:");
+        try (PrintWriter outputFile = new PrintWriter(scanner.nextLine())) {
+            int cardCount = 0;
+            for (var card : cardMap.entrySet()) {
+                outputFile.printf("%s | %s\n", card.getKey(), card.getValue());
+                cardCount++;
+            }
+            System.out.printf("%d cards have been saved.\n\n", cardCount);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void getCards() {
-        //TODO: implement ask
+        System.out.println("How many times to ask?");
+        int rounds = Integer.parseInt(scanner.nextLine());
+        int i = 0;
+        String answer;
+
+        for (var entry : cardMap.entrySet()) {
+            if (i < rounds) {
+                System.out.printf("Print the definition of \"%s\":\n", entry.getKey());
+                answer = scanner.nextLine();
+
+                if (entry.getValue().equals(answer)) {
+                    System.out.println("Correct!");
+                } else if (cardMap.containsValue(answer)) {
+                    System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n",
+                            entry.getValue(), keyMap.get(answer));
+                } else {
+                    System.out.printf("Wrong. The right answer is \"%s\".\n", entry.getValue());
+                }
+            }
+            i++;
+        }
     }
 }
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,47 +1,7 @@
 package flashcards;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Input the number of cards:");
-        int cards = Integer.parseInt(scanner.nextLine());
-        String term;
-        String definition;
-        String answer;
-
-        for (int i = 1; i <= cards; i++) {
-            System.out.printf("Card #%d:\n", i);
-            term = scanner.nextLine();
-
-            while (cardMap.containsKey(term)) {
-                System.out.printf("The term \"%s\" already exists. Try again:\n", term);
-                term = scanner.nextLine();
-            }
-
-            System.out.printf("The definition for card #%d:\n", i);
-            definition = scanner.nextLine();
-
-            while (cardMap.containsValue(definition)) {
-                System.out.printf("The definition \"%s\" already exists. Try again:\n", definition);
-                definition = scanner.nextLine();
-            }
-            cardMap.put(term, definition);
-            keyMap.put(definition, term);
-        }
-
-        for (var entry : cardMap.entrySet()) {
-            System.out.printf("Print the definition of \"%s\":\n", entry.getKey());
-            answer = scanner.nextLine();
-
-            if (entry.getValue().equals(answer)) {
-                System.out.println("Correct!");
-            } else if (cardMap.containsValue(answer)) {
-                System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n",
-                        entry.getValue(), keyMap.get(answer));
-            } else {
-                System.out.printf("Wrong. The right answer is \"%s\".\n", entry.getValue());
-            }
-        }
     }
 }
